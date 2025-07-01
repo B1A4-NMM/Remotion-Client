aee201480b36e6c9dacb93d3285a85f61605b3b0
오수빈 <catherine621@naver.com>
2025-07-01 16:30:21 +0900

    🐛fix[*]:diary post api 연결

--- src/api/queries/diary/usePostDiary.ts
+++ src/api/queries/diary/usePostDiary.ts
@@ -4,5 +4,11 @@ import { postDiary } from "./../../services/diary";
 export const usePostDiary = () => {
   return useMutation({
     mutationFn: postDiary,
+    onSuccess: data => {
+      console.log("일기 업로드 성공", data);
+    },
+    onError: error => {
+      console.error(" 일기 업로드 실패", error);
+    },
   });
 };

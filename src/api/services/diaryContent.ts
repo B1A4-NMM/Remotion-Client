16996767214558f5ddc34cddb88c9d04d7f3b5cb
pqud <49116761+pqud@users.noreply.github.com>
2025-07-04 20:48:54 +0900

    🎨 style[home.tsx]: 홈 화면 수정

---
+++ src/api/services/diaryContent.ts
@@ -0,0 +1,16 @@
+// api/services/diaryContent.ts
+import axios from "axios";
+import type { DiaryResponse } from "../../types/diary";
+
+export const getDiaryContent = async (token: string, id: string): Promise<DiaryResponse> => {
+  const BASE_URL = import.meta.env.VITE_SOCIAL_AUTH_URL;
+
+  const response = await axios.get(`${BASE_URL}/diary/${id}`, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return response.data;
+};

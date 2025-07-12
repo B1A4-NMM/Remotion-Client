f0daba18dfebb75799b9bb40c7305e922dc0f245
molcham <130418659+molcham@users.noreply.github.com>
2025-07-12 22:40:16 +0900

    🐛 FIX[routine]: routine 폴더  UI 구현중

--- src/pages/Routine.tsx
+++ src/pages/Routine.tsx
@@ -1,14 +1,30 @@
 import Title from "@/components/recommend/Title";
+import FolderCardList from "@/components/routine/FolderCardList";
+
 
 const Routine=()=>{
 
     return(
-        <div className="relative flex flex-col h-screen border">
+
+        <div >
             <Title/>
 
-            <div className="flex justify-between">
+            <div className="mt-[78px] flex justify-between px-7">
+                <h1 className="text-xl font-bold mb-[20px] ">당신의 루틴</h1>
+
             </div>
+
+            <FolderCardList/>
+
+            <div className="flex justify-between px-7">
+                <h1 className="text-xl font-bold ">테마 별 루틴 </h1>
+            </div>
+
+            {/* <Video/> */}
+
         </div>
+        
+        
     );
 }
 

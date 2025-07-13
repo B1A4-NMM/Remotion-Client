04eb0eb176aff6cff26ad10b7e752c4bdab37842
molcham <130418659+molcham@users.noreply.github.com>
2025-07-13 17:09:20 +0900

    ✨ feat[routine] : 루틴 추천 페이지 1차완성본

--- src/components/routine/FolderCard.tsx
+++ src/components/routine/FolderCard.tsx
@@ -1,30 +1,34 @@
-
 interface FolderCardProps {
-  title: string;
-  subtitle: string;
-  imageSrc: string;
+    title: string;
+    subtitle: string;
+    imageSrc: string;
+  }
   
-}
-
-const FolderCard = ({ title, subtitle, imageSrc}: FolderCardProps) => {
-  return (
-    <div className="relative min-w-[240px] h-[160px] mr-4" >
-      <img
-        src={imageSrc}
-        alt={title}
-        className="absolute inset-0 w-full h-full object-cover"
-        style={{ borderRadius: "0"}} 
-      />
-
-      <div className="absolute top-5 left-5 text-white text-sm font-semibold">
-        {title}
-        <div className="text-xs mt-1 font-normal">{subtitle}</div>
+  const FolderCard = ({ title, subtitle, imageSrc }: FolderCardProps) => {
+    return (
+      <div className="relative min-w-[240px] h-[160px] mr-4 rounded-[24px] overflow-hidden ">
+        {/* 배경 이미지 */}
+        <img
+          src={imageSrc}
+          alt={title}
+          className="absolute inset-0 w-full h-full object-cover"
+        />
+  
+        {/* 텍스트 */}
+        <div className="absolute top-[40px] left-[20px] text-black">
+          <div className="text-[14px] font-medium leading-tight">{title}</div>
+          <div className="text-[12px] font-light leading-snug mt-[6px] text-white whitespace-pre-wrap">
+            {subtitle}
+          </div>
+        </div>
+  
+        {/* 플러스 버튼 */}
+        <button className="absolute bottom-[20px] right-[25px] w-8 h-8 bg-white/40 rounded-full text-white text-xl flex items-center justify-center">
+          +
+        </button>
       </div>
-      <button className="absolute bottom-5 right-5 w-7 h-7 bg-white/40 rounded-full text-white text-xl flex items-center justify-center">
-        +
-      </button>
-    </div>
-  );
-};
-
-export default FolderCard;
+    );
+  };
+  
+  export default FolderCard;
+  

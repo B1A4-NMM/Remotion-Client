9e7673463532f592ca6feb5d274eb27e8121e147
오수빈 <catherine621@naver.com>
2025-07-16 01:23:34 +0900

    ✨ feat[login] : 데모 로그인 연결

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,6 +3,7 @@ import kakao from "./../assets/img/kakao.svg";
 import google from "./../assets/img/google.svg";
 import { Canvas } from "@react-three/fiber";
 import SimpleBlob from "@/components/Blob/Simple/SimpleBlob";
+import { demoLogin } from "@/api/services/auth";
 const BASE_URL = import.meta.env.VITE_SOCIAL_AUTH_URL;
 
 const SOCIAL_AUTH_URL = {
@@ -13,15 +14,14 @@ export default function Login() {
   return (
     <div className=" min-h-screen h-full flex flex-col justify-center items-center px-4">
       <h1 className="text-2xl font-bold mb-6 text-black text-left">
-        하루를 기록하면 <br/>
+        하루를 기록하면 <br />
         내일이 달라지는 다이어리
       </h1>
 
       {/* 로고 */}
       <div className="w-40 h-40 m-10">
-        <Canvas
-           camera={{ position: [0, 0, 3], fov: 90 }}>
-          <SimpleBlob/>
+        <Canvas camera={{ position: [0, 0, 3], fov: 90 }}>
+          <SimpleBlob />
         </Canvas>
       </div>
 
@@ -42,8 +42,26 @@ export default function Login() {
           <img src={google} alt="Google Icon" className="w-5 h-5" />
           <span>구글로 계속하기</span>
         </Button>
+
         <Button
-          onClick={() => (window.location.href = SOCIAL_AUTH_URL.google)}
+          onClick={async () => {
+            try {
+              const res = await demoLogin();
+              console.log(res);
+              // 응답 구조에 따라 token 추출 (예: res.token)
+              const token = res.accessToken;
+              console.log(token);
+              if (token) {
+                localStorage.setItem("accessToken", token);
+                window.location.href = "/";
+              } else {
+                alert("토큰을 받아오지 못했습니다.");
+                console.log(token);
+              }
+            } catch (e) {
+              alert("데모 로그인 실패");
+            }
+          }}
           className="h-[48px] bg-white text-black hover:bg-gray-200 rounded-full justify-center items-center gap-3 px-4 py-2"
         >
           <span>하루뒤 둘러보기</span>

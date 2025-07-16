56897b98cb3a68e6d91face43c0de217300fa705
juniper-j <jy.juniper.jeong@gmail.com>
2025-07-16 21:31:44 +0900

    🐛 fix[*]: 캐릭터 카드, 바텀네비게이션

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,31 +1,27 @@
-import Home from "./pages/Home";
-
-import Calendar from "./pages/Calendar";
-import Diary from "./pages/Diary";
-import Relation from "./pages/Relation";
-import RelationDetail from "./pages/RelationDetail";
-
-import Result from "./pages/Result";
-import Test from "./pages/Test";
-import TestResult from "./components/TestResult";
 import Login from "./pages/Login";
 import GetAccess from "./pages/GetAccess";
+
+import Home from "./pages/Home";
+import Diary from "./pages/Diary";
+import SearchPage from "./pages/SearchPage";
+import Result from "./pages/Result";
 import Loading6 from "./components/Loading/Loading6";
-import Analysis from "./pages/Analysis";
-import AboutMe from "./pages/AboutMe";
-import Map from "./pages/Map";
+
 import Video from "./pages/Video";
 import Routine from "./pages/Routine";
 import Contents from "./pages/Contents";
-import SearchPage from "./pages/SearchPage";
 
+import Analysis from "./pages/Analysis";
 import Strength from "./pages/analysis/Strength";
 import Stress from "./pages/analysis/Stress";
 import Anxiety from "./pages/analysis/Anxiety";
 import Depress from "./pages/analysis/Depress";
 import Character from "./pages/analysis/Character";
+import Relation from "./pages/Relation";
+import RelationDetail from "./pages/RelationDetail";
 
-import Action from "./pages/Action";
+import Calendar from "./pages/Calendar";
+
 export const routes = [
   { path: "/", element: <Home /> },
   { path: "/diary/:date", element: <Diary /> },
@@ -33,16 +29,11 @@ export const routes = [
   { path: "/relation", element: <Relation /> },
   { path: "/relation/:id", element: <RelationDetail /> },
   { path: "/result/:id", element: <Result /> },
-  { path: "/test", element: <Test /> },
-  { path: "/test/result", element: <TestResult /> },
   { path: "/login", element: <Login /> },
   { path: "/getAccess", element: <GetAccess /> },
   { path: "/loading", element: <Loading6 /> },
   { path: "/analysis", element: <Analysis /> },
-  { path: "/aboutme", element: <AboutMe /> },
-  { path: "/map", element: <Map /> },
   { path: "/video", element: <Video /> },
-  { path: "/action", element: <Action /> },
   { path: "/routine", element: <Routine /> },
   { path: "/contents", element: <Contents /> },
   { path: "/search", element: <SearchPage /> },

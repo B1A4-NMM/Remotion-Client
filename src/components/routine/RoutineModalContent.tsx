485c3bb6b438af76924743841d7d28a7e6901f1a
molcham <130418659+molcham@users.noreply.github.com>
2025-07-22 20:19:13 +0900

    🐛 fix[routine]: 루틴 폴더 안에서 스크롤 기능 수정

--- src/components/routine/RoutineModalContent.tsx
+++ src/components/routine/RoutineModalContent.tsx
@@ -45,29 +45,39 @@ const RoutineModalContent = ({
       {/* ✅ 루틴 리스트만 스크롤되게 분리 */}
 
       {routines.length === 0 ? (
-        <p className="text-sm text-gray-400 dark:text-white/50">아직 추가된 루틴이 없어요.</p>
-      ) : (
-        routines.map(routine => (
-          <div
-            key={routine.id}
-            className="flex justify-between items-center border px-4 py-3 rounded-xl mb-2 
-          text-sm transition
-          dark:border-white/30 border-gray-300 
-          dark:text-white text-black 
-          dark:bg-white/10 bg-white 
-          dark:hover:bg-white/20 hover:bg-gray-100
-          backdrop-blur-sm"
-          >
-            <span className="text-sm text-black dark:text-white font-sans">{routine.content}</span>
-            <button
-              onClick={() => onDelete(routine.id)}
-              className="text-xs dark:text-red-300 dark:hover:text-red-500 text-red-600 hover:text-red-800"
-            >
-              delete
-            </button>
-          </div>
-        ))
-      )}
+    <p className="text-sm text-gray-400 dark:text-white/50">
+      아직 추가된 루틴이 없어요.
+    </p>
+) : (
+  <div className="flex-1 overflow-y-auto pr-1 mb-2">
+    {routines.map(routine => (
+      <div
+        key={routine.id}
+        className="flex justify-between items-center
+        border px-4 py-3 rounded-xl mb-2 
+        text-sm transition
+        dark:border-white/10 border-gray-300 
+        dark:text-white text-black 
+        dark:bg-white/ bg-white 
+        dark:hover:bg-white/10 hover:bg-gray-100
+        backdrop-blur-sm"
+      >
+        <span className="text-sm text-black dark:text-white font-sans">
+          {routine.content}
+        </span>
+        <button
+          onClick={() => onDelete(routine.id)}
+          className="text-xs dark:text-red-300 dark:hover:text-red-500 text-red-600 hover:text-red-800"
+        >
+          delete
+        </button>
+      </div>
+    ))}
+  </div>
+)}
+
+
+     
 
       {/* ✅ 항상 하단에 고정되는 추가 입력창 */}
       <div className="flex mt-4 gap-2 pt-2 border-t border-gray-300 dark:border-white/20">
